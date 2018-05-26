8adfe77f723d673c14cbe003eda8d2226860f876
kezhenxu94 <kezhenxu94@163.com>
2018-05-26 09:42:55 +0800

    Fix Issue #4

--- crawler/house_renting/spiders/lianjia.py
+++ crawler/house_renting/spiders/lianjia.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import json
+
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
 from scrapy.spiders import Rule
@@ -6,16 +8,34 @@ from scrapy.spiders import Rule
 from house_renting.base_spider import BaseCrawlSpider
 from house_renting.items import HouseRentingLianjiaItem
 
+current_page = 0
+
 
 class LianjiaSpider(BaseCrawlSpider):
     name = 'lianjia'
     allowed_domains = ['lianjia.com']
 
     rules = (
-        Rule(LinkExtractor(allow=r'/zufang/(pg\d+/)?$', restrict_css=('div.list-wrap > ul > li', 'div.page-box')),
-             follow=True),
+        Rule(LinkExtractor(allow=r'/zufang/(pg\d+/)?$', restrict_css='div.list-wrap > ul > li'), follow=True),
         Rule(LinkExtractor(allow=r'/zufang/\w+.html$'), callback='parse_item'),
     )
+
+    def parse_start_url(self, response):
+        page_data = response.css('div.page-box::attr(page-data)').extract_first()
+        if page_data is None:
+            return
+
+        page_data = json.loads(page_data)
+        total_page = page_data['totalPage']
+        if total_page is None:
+            return
+
+        page_url_pattern = response.css('div.page-box::attr(page-url)').extract_first()
+        if page_url_pattern is None:
+            return
+
+        for page in range(0, total_page):
+            yield response.follow(page_url_pattern.replace('{page}', str(page)))
 
     def parse_item(self, response):
         item_loader = ItemLoader(item=HouseRentingLianjiaItem(), response=response)
