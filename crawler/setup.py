035e15e165ca8a0ba5627b373f165a4955a21e16
kezhenxu94 <kezhenxu94@163.com>
2018-06-02 18:19:57 +0800

    Optimize docker-compose

--- crawler/setup.py
+++ crawler/setup.py
@@ -6,6 +6,6 @@ setup(
     name='project',
     version='1.0',
     packages=find_packages(),
-    entry_points={'scrapy': ['settings = house_renting.settings']}, install_requires=['scrapy', 'elasticsearch',
-                                                                                      'redis']
+    entry_points={'scrapy': ['settings = house_renting.settings']},
+    install_requires=['scrapy', 'elasticsearch', 'redis']
 )
